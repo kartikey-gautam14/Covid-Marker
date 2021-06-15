a1bec299b39bd9ad67571c7445d589075b1988b6
kartikey gautam <kartikeygautam14@gmail.com>
2021-06-15 20:55:05 +0530

    drop down added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState,useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core"
 import './App.css';
 
 function App() {
+  
+  const [countries,setCountries] = useState([]);
+  const [country,setCountry] =useState("worldwide");
+  useEffect(()=>{
+
+    const getcountryData = async ()=>{
+      fetch("https://disease.sh/v3/covid-19/countries")
+      .then(response=>response.json())
+      .then((data)=>{
+        const countries = data.map((country)=>({
+          name: country.country,
+          value: country.countryInfo
+        }))
+        setCountries(countries);
+      })
+    }
+
+    getcountryData();
+
+  },[])
+
+  const countryChange = ((event)=>{
+    const countryCode=event.target.value; 
+    setCountry(countryCode);
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        <h1>Covid Marker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={countryChange} value={country}>
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+          {countries.map(country=>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+
+
+            {/* <MenuItem value="uk">uk</MenuItem>
+            <MenuItem value="uk">uk</MenuItem>
+            <MenuItem value="uk">uk</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+
     </div>
   );
 }
